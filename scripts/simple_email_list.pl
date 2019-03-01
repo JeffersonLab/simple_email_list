7679328dd5103892b65eef49f0ad836132847b1d
markito3 <marki@jlab.org>
2019-02-28 19:17:30 -0500

    initial commit from subversion: scripts/simple_email_list to git,

---
+++ scripts/simple_email_list.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/env perl
+$message_length = `wc -c message.txt`;
+if ($message_length == 0) {
+    print "warning: empty message, no email sent\n";
+    exit 0;
+}
+open(SUBJ, "subject.txt") or die "error: subject.txt not found";
+$subject = <SUBJ>;
+chomp $subject;
+system "svn update";
+open(LIST, "list.txt") or die "error: list.txt not found";
+$addressees = '';
+while (<LIST>) {
+    chomp;
+    $addressees .= ' ' . $_;
+}
+system "mail -s \"$subject\" $addressees < message.txt\n";
+system "rm message.txt ; touch message.txt";
+exit;
